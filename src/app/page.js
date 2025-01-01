3c0f1ebd64b999ffa37fa7328b3c351952f811e1
Weylin Morris <hi@weylin.dev>
2025-01-01 17:17:20 -0600

    Mobile frinedly somewhat

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,23 @@
 import {BlockProvider} from "@/context/block";
 import Main from "@/components/homepage/Main.jsx";
 import Sidebar from "@/components/homepage/Sidebar.jsx";
-import Recommended from "@/components/homepage/Recommended.jsx";
+import Recommended from "@/components/homepage/RightSidebar.jsx";
 import {ToastProvider} from "@/context/toast/ToastContext.js";
 
 export default function Home() {
-  return (
-      <BlockProvider>
-          <ToastProvider>
-              <div className="flex items-stretch h-full">
-                  <Sidebar/>
-                  <Main/>
-                  <Recommended/>
-              </div>
-          </ToastProvider>
-      </BlockProvider>
-  );
+    return (
+        <BlockProvider>
+            <ToastProvider>
+                <div className="flex items-stretch h-full">
+                    <div className="hidden md:block">
+                        <Sidebar/>
+                    </div>
+                    <Main/>
+                    <div className="hidden md:block">
+                        <Recommended/>
+                    </div>
+                </div>
+            </ToastProvider>
+        </BlockProvider>
+    );
 }
